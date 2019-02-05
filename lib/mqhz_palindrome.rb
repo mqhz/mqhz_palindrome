4375f2b0a29913fabd1b64c8993f8e883b4cec5f
QUAYAM <quayamzaidi2010@gmail.com>
2019-02-06 02:01:47 +0530

    Initializing Repository

---
+++ lib/mqhz_palindrome.rb
@@ -0,0 +1,6 @@
+require "mqhz_palindrome/version"
+
+module MqhzPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mqhz_palindrome/version.rb
@@ -0,0 +1,3 @@
+module MqhzPalindrome
+  VERSION = "0.1.0"
+end
